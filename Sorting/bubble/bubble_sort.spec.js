013ad62a33e07fb265fcca3490a2f39a93d0cdde
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-07 22:07:01 +1000

    added test

--- Sorting/bubble/bubble_sort.spec.js
+++ Sorting/bubble/bubble_sort.spec.js
@@ -8,5 +8,9 @@ describe("Bubble sort", () => {
 
     it("should sort [1000,7,21,6,8,793,99,9] array", () => {
         expect(BubbleSort([1000,7,21,6,8,793,99,9])).toEqual([6,7,8,9,21,99,793,1000]);
+    });
+
+    it("should sort [0, -99, 2, -1, -5, 10] array", () => {
+        expect(BubbleSort([0, -99, 2, -1, -5, 10])).toEqual([-99, -5, -1, 0, 2, 10]);
     })
 })
