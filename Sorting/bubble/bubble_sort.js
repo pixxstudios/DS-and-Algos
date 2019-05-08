3b74a881065998a0f7ef741c9fe3d02484b2f7c1
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-08 20:51:51 +1000

    code update

--- Sorting/bubble/bubble_sort.js
+++ Sorting/bubble/bubble_sort.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var BubbleSort = function (arr) {
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < arr.length; i++) {
         for (var j = 0; j < arr.length; j++) {
             console.log(arr[j], arr[j + 1]);
             if (arr[j] > arr[j + 1]) {
