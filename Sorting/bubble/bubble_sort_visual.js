c183e8251d9df34a766e87c59eb60707dcfbebc8
Gagandeep Singh <gagan.ddun@gmail.com>
2019-05-08 21:20:48 +1000

    code update for visualisation

--- Sorting/bubble/bubble_sort_visual.js
+++ Sorting/bubble/bubble_sort_visual.js
@@ -1,15 +1,36 @@
 let arr;
+let i = 0;
+let j = 0;
 
 function setup() {
     createCanvas(600, 300);
     arr = new Array(width);
     
     for(let i = 0 ; i < arr.length; i++)
-        arr[i] = random(height);
+        arr[i] = random(height);    
 }
 
 function draw() {
     background(0);
+
+    let a = arr[j];
+    let b = arr[j+1];
+
+    if (a > b) {
+        var temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+    }
+
+    j++;
+
+    if(i < arr.length) {
+        if( j >= arr.length-i-1) {
+            j = 0;
+            i++;
+        }
+    }
+    
     for(let i = 0 ; i < arr.length; i++) {
         stroke('yellow');
         line(i, height, i , height - arr[i]);
