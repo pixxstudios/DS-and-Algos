c459ca274a19cc852c535966808b134362455149
Gagandeep Singh <gagan.ddun@gmail.com>
2019-07-05 22:40:46 +1000

    lastNode

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -16,6 +16,14 @@ class LinkedList {
         this.head = new Node(data, this.head);
     }
 
+    // insert last node
+    insertLast(data) {
+        let current = this.head;
+        if(!this.head) this.head = new Node(data);
+        else while(current.next) current = current.next;
+        current.next = new Node(data);
+    }
+
     // print linked list
     printListData() {
         let current = this.head;
@@ -29,5 +37,7 @@ class LinkedList {
 const ll = new LinkedList();
 ll.insertFirst(100);
 ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(500);
 
 ll.printListData();
